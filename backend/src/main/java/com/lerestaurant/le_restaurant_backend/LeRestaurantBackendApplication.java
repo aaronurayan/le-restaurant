bfff67e18ac85e1ea08e6664690c9c0da4e73f87
salieri009 <salieri009@example.com>
2025-09-02 12:46:48 +1000

    fix

---
+++ backend/src/main/java/com/lerestaurant/le_restaurant_backend/LeRestaurantBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lerestaurant.le_restaurant_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeRestaurantBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeRestaurantBackendApplication.class, args);
+	}
+
+}
