02ee1c0711706f3637b5a71468a89b9571c3c9e3
salieri009 <salieri009@example.com>
2025-10-01 17:06:41 +1000

    Fix Azure pipeline: add test:ci script and configure Java 17

--- backend/src/main/java/com/lerestaurant/le_restaurant_backend/controller/UserController.java
+++ backend/src/main/java/com/lerestaurant/le_restaurant_backend/controller/UserController.java
@@ -1,18 +1,27 @@
 package com.lerestaurant.le_restaurant_backend.controller;
 
-import com.lerestaurant.le_restaurant_backend.dto.UserDto;
-import com.lerestaurant.le_restaurant_backend.dto.UserCreateRequestDto;
-import com.lerestaurant.le_restaurant_backend.dto.UserUpdateRequestDto;
-import com.lerestaurant.le_restaurant_backend.entity.User;
-import com.lerestaurant.le_restaurant_backend.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lerestaurant.le_restaurant_backend.dto.UserCreateRequestDto;
+import com.lerestaurant.le_restaurant_backend.dto.UserDto;
+import com.lerestaurant.le_restaurant_backend.dto.UserUpdateRequestDto;
+import com.lerestaurant.le_restaurant_backend.entity.User;
+import com.lerestaurant.le_restaurant_backend.service.UserService;
 
 @RestController
 @RequestMapping("/api/users")
