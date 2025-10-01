c4388021dc63ccae90b2a4dd5a47fdf7f60a472b
salieri009 <kordalek@naver.com>
2025-10-01 17:06:41 +1000

    Fix Azure pipeline: add test:ci script and configure Java 17

--- backend/src/main/java/com/lerestaurant/le_restaurant_backend/controller/PaymentController.java
+++ backend/src/main/java/com/lerestaurant/le_restaurant_backend/controller/PaymentController.java
@@ -1,17 +1,27 @@
 package com.lerestaurant.le_restaurant_backend.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.lerestaurant.le_restaurant_backend.dto.PaymentDto;
 import com.lerestaurant.le_restaurant_backend.dto.PaymentRequestDto;
 import com.lerestaurant.le_restaurant_backend.entity.Payment;
 import com.lerestaurant.le_restaurant_backend.service.PaymentService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/payments")
